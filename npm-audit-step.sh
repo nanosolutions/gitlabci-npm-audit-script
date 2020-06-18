21e7d7b3e3281f4db9d9c442c3f2d6686b68bcee
Petr Cervenka <cerwenka@gmail.com>
2020-06-18 09:23:45 +0800

    Update npm-audit-step.sh

--- npm-audit-step.sh
+++ npm-audit-step.sh
@@ -1,20 +1,48 @@
 #!/bin/bash
 
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        COMMAND="jq-linux64"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        COMMAND="jq-osx-amd64"
+fi
+
+GITHUB="https://github.com/stedolan/jq/releases/download/jq-1.6/"
+
 print_vulnerabilities () {
-    ./jq-linux64 .metadata.vulnerabilities < ./audit_result.json
-    cat ./audit_result.json
+    
+    echo "Report"
+    npm audit
 }
 
-wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-chmod +x jq-linux64
-npm i --package-lock-only
+# print_vulnerabilities () {
+#     echo "Summary "
+#     ./${COMMAND} .metadata.vulnerabilities < ./audit_result.json
+#     echo "Report"
+#     ./${COMMAND} < ./audit_result.json
+# }
+
+if [ ! -f "$COMMAND" ]; then
+    wget -q  $GITHUB$COMMAND
+fi
+
+
+chmod +x $COMMAND
+#npm i --package-lock-only
 npm audit --json > audit_result.json
 
-INFO_VUL="$(./jq-linux64 .metadata.vulnerabilities.info < ./audit_result.json)"
-LOW_VUL="$(./jq-linux64 .metadata.vulnerabilities.low < ./audit_result.json)"
-MODERATE_VUL="$(./jq-linux64 .metadata.vulnerabilities.moderate < ./audit_result.json)"
-HIGH_VUL="$(./jq-linux64 .metadata.vulnerabilities.high < ./audit_result.json)"
-CRITICAL_VUL="$(./jq-linux64 .metadata.vulnerabilities.critical < ./audit_result.json)"
+#INFO_VUL="$(./${COMMAND} .metadata.vulnerabilities.info < ./audit_result.json)"
+#LOW_VUL="$(./${COMMAND} .metadata.vulnerabilities.low < ./audit_result.json)"
+MODERATE_VUL="$(./${COMMAND} .metadata.vulnerabilities.moderate < ./audit_result.json)"
+HIGH_VUL="$(./${COMMAND} .metadata.vulnerabilities.high < ./audit_result.json)"
+CRITICAL_VUL="$(./${COMMAND} .metadata.vulnerabilities.critical < ./audit_result.json)"
+
+SUMMARY_VUL="$(./${COMMAND} .metadata.vulnerabilities < ./audit_result.json)"
+
+
+echo "Summary "
+./${COMMAND} .metadata.vulnerabilities < ./audit_result.json
+
 
 # if [ "$INFO_VUL" -ne "0" ]
 # then
